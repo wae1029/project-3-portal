c227bbde81cc7b7b0e1808e09766f98068bd0dae
William <sechswilliam@gmail.com>
2022-04-30 14:32:32 -0500

    Start from scratch make teleporter

---
+++ Assets/Scripts/RayCast.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+    //fire range 
+    public float range = 20f;
+    public GameObject prefab;
+    //fire and impact effect 
+    //public ParticleSystem fire;
+    //public GameObject impactEffect;
+    [SerializeField] LayerMask layerMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update() { 
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    
+    void Shoot() {
+
+        //particle effect on fire
+        //fire.Play();
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, range, layerMask))
+            {
+            //Destroy(prefab);
+            Instantiate(prefab, hit.point, Quaternion.identity);
+            Debug.Log("Hit something");
+            }
+        else
+            {
+    Debug.Log("Hit nothing");
+    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 20f, Color.green);
+
+            }
+        //particle effect on inpact
+        //GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        //Destroy(impactGO, 2f);
+    }
+}
---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject TeleportTo;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Player.transform.position = TeleportTo.transform.position;
+            Destroy(gameObject);
+        }
+    }
+}
